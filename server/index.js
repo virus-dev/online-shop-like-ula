7f36ec663bb491ab82b30cb1394db9f818b3caa7
Nikita <nikita-pashin12@yandex.ru>
2022-03-21 13:30:25 +0500

    eee

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const router = require('./routes/index');
 
 const app = express();
 app.use('/api', router);
+app.get('/', (req, res) => res.json({ message: 'Hidwadwadwa' }));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 
 const start = async () => {
   try {
-    app.listen(PORT, () => console.log('GOOOOOO'))
+    app.listen(PORT, () => console.log('========================', PORT));
   } catch (e) {
     console.log('Error')
   }
