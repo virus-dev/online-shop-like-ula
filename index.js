aca01ee0cfea11a217482de7a4250caaa2f8d497
Nikita <nikita-pashin12@yandex.ru>
2022-03-21 15:48:12 +0500

    ___

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const start = require('./server/index');
+const express = require('express');
+const router = require('./routes/index');
 
-start(process.env.PORT || 8000);
+const app = express();
+app.use('/api', router);
+app.get('/', (req, res) => res.json({ message: 'Hidwadwadwa' }));
+
+const PORT = process.env.PORT || 5000;
+
+const start = () => {
+  try {
+    app.listen(PORT, () => console.log('Server started on PORT:', PORT));
+  } catch (e) {
+    console.log(e)
+  }
+}
+start();
